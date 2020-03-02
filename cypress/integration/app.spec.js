a7a6e65a15b2b3cd8fb862eb9aab610bd7eadea6
Danny Michaelis <dmichaelis0@gmail.com>
2020-03-01 20:32:09 -0500

    add cypress add initial snapshot tests

---
+++ cypress/integration/app.spec.js
@@ -0,0 +1,28 @@
+describe('Snapshot Testing', () => {
+    beforeEach(() => {
+        cy.visit('http://localhost:3000/');
+        cy.contains('YOUR EXERCISES', { timout: 30000 });
+    });
+    it('should display the app', () => {
+        cy.get('.App').toMatchImageSnapshot({
+            threshold: 0.1,
+        })
+    });
+    describe('Mobile', () => {
+        beforeEach(() => {
+            cy.viewport('iphone-6')
+        });
+        it('should display the app', () => {
+            cy.get('.App').toMatchImageSnapshot({
+                threshold: 0.1,
+            })
+        });
+        it('should reveal the sidebar when you click on the menu button', () => {
+            cy.click('.show-menu-button');
+            cy.wait(1000);
+            cy.get('.App').toMatchImageSnapshot({
+                threshold: 0.1,
+            })
+        });
+    })
+});
